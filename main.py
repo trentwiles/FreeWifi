fe5aa135f1dd0151335b2971fbd198463bcd6d36
trentwiles <wilest7@gmail.com>
2023-07-23 14:42:22 -0400

    more framing

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+@app.route('/map/<lng>/<lat>')
+def mapService(lng, lat):
+    return render_template('mapRegion.html', lng=float(lng), lat=float(lat))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
